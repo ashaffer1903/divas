7115fe4d8d07d2001ca994da33f7b54faa7adda2
Andrew Shaffer <shaffer@Andrews-MacBook-Pro.local>
2017-11-02 20:04:03 -0400

    Initial commit of repo

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template lang="pug">
+  section
+    div(id="app")
+      <!--img(src="./assets/logo.png")-->
+      router-view
+    div.footer(id="footer")
+      div.container
+        div.columns.is-centered
+          div.column.is-one-third
+            table
+              tbody
+                tr
+                  td
+                    b-icon(icon="phone")
+                  td(style="padding-left: 15px") (978) 466-5969
+                tr
+                  td
+                    b-icon(icon="envelope-open-o")
+                  td(style="padding-left: 15px") divas@divashd.com
+          div.column.is-one-third
+            table(align="center")
+              tbody
+                tr
+                  td
+                    a.has-text-info(href="https://www.facebook.com/DivasHD1" target="_blank")
+                      b-icon(icon="facebook-square" size="is-medium")
+                tr
+                  td
+                    b-icon(icon="twitter-square" size="is-medium")
+                tr
+                  td
+                    a.has-text-primary(href="https://www.instagram.com/divas_hd/" target="_blank")
+                      b-icon(icon="instagram" size="is-medium")
+                tr
+                  td
+                    a.has-text-danger(href="https://www.pinterest.com/divashairdesign" target="_blank")
+                      b-icon(icon="pinterest-square" size="is-medium")
+          div.column.is-one-third
+            table(align="center")
+              tbody
+                tr
+                  td(style="text-align: center")
+                    b-icon.has-text-danger(icon="map-pin" size="is-medium")
+                tr
+                  td(style="text-align: center") 797 Union Street
+                tr
+                  td(style="text-align: center") Leominster, Ma 01453
+
+
+        div.content.has-text-centered
+
+        div(style="padding-top: 15px")
+          table(align="center")
+            tbody
+              tr
+                td
+                  i.fa.fa-copyright
+                td(style="padding-left: 15px") 2017 | Divas Hair Design
+</template>
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style lang="sass">
+  @import "~bulma"
+  @import "~buefy/src/scss/buefy"
+  @import "../node_modules/font-awesome/css/font-awesome.min.css"
+
+  .app
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,63 @@
+<template lang="pug">
+  section
+    article
+      nav.level
+        p.level-item.has-text-centered
+          a.link.is-info Home
+        p.level-item.has-text-centered
+          a.link.is-info About
+        p.level-item.has-text-centered.is-uppercase.is-size-2 Divas
+          span.has-text-grey HD
+        p.level-item.has-text-centered
+          a.link.is-info Services
+        p.level-item.has-text-centered
+          a.link.is-info Contact
+
+    article.hero.is-medium.is-primary.is-bold(style="margin-top: 20px")
+      div.hero-body
+        div.container
+          div.columns.is-centered
+            div.column.is-half
+              table(align="center")
+                tbody
+                  tr
+                    td.title We
+                    td(style="padding-left: 5px")
+                      b-icon.is-danger.is-medium(icon="heart")
+                    td.title(style="padding-left: 5px") Hair
+          p.is-size-5.has-text-centered(style="padding-top: 30px") Welcome to Divas Hair Design! You have come to the right place for great hair.
+          p.is-size-5.has-text-centered(style="padding-top: 30px") Sit back. Relax. And let us take care of you.
+
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
